aa8b95c6d373ed5ecfa10a446f978b2e8c9dd183
Cute Puppy Dduo <164348579+Dddddduo@users.noreply.github.com>
2024-11-29 13:55:41 +0800

    完善配置文件的问题 可以合并

--- src/main/java/com/itheima/ReggieApplication.java
+++ src/main/java/com/itheima/ReggieApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ServletComponentScan
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
+// 指定配置文件启动
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
